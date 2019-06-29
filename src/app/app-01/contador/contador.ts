fe1c4613e4e248ae05c15c60282cb33f12630a96
Leonor Guzman Diaz <leonor.guzman@invisiblebits.com>
2019-06-30 00:41:05 +0200

    update
    update

--- src/app/app-01/contador/contador.ts
+++
@@ -1,3 +0,0 @@
-export interface IContador {
-  contador: number;
-}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,11 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import {App01Routing} from './app-01/app-01.routing';
-import {TodoAppRouting} from './todo-app/todo-app.routing';
 
 
 const routes: Routes = [
   {
-    path: 'app-01',
-    loadChildren: () => App01Routing
-  },
-  {
     path: '',
-    loadChildren: () => TodoAppRouting
+    loadChildren: () => import('src/app/todo-app/todo-app.routing').then(mod => mod.TodoAppRouting)
   }
 ];
 
